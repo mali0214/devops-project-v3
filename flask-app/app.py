0dafd690ba8cbc3c82699b790edb29740cad057b
Mehmet ali Karaçalı <m.karacali98@icloud.com>
2025-05-05 17:02:10 +0300

    Update app routes to match test cases

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, jsonify, request
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello from Flask inside Docker on EC2!'
+def home():
+    return 'Welcome to the Flask App!'
+
+@app.route('/another-route')
+def another_route():
+    return 'Another page'
+
+@app.route('/some-post-endpoint', methods=['POST'])
+def post_endpoint():
+    data = request.get_json()
+    # You can handle the data here if needed
+    return jsonify({"message": "Success"}), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
-
